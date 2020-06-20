a2f04b2a927fca0c5cb3c2678f3330f574befd5d
Michael Currin <michael.currin@gmail.com>
2020-06-20 21:33:29 +0200

    feat: Create script

---
+++ genesis/full-upgrade.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# full-upgrade
+#       performs the function of upgrade but will remove currently installed packages if this is needed
+#       to upgrade the system as a whole.
+
+echo 'Upgrade packages and remove packages if needed'
+echo
+
+set -e
+
+sudo apt update
+
+sudo apt full-upgrade -y

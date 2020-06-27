31ce829efe5bfdd7259abce5027b561f4c7750aa
Michael Currin <michael.currin@gmail.com>
2020-06-27 20:19:24 +0200

    feat: Add deno

--- genesis/config.sh
+++ genesis/config.sh
@@ -3,23 +3,9 @@
 echo 'Configure system'
 echo
 
-set -e
+set -e -x
 
-# Node.js / NPM
-
-# Setup Debian repo for Node.js
-# https://nodejs.org/en/download/package-manager/
-# https://github.com/nodesource/distributions/blob/master/README.md
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-
-# Output from above:
-#
-# ## You may also need development tools to build native addons:
-#      sudo apt-get install gcc g++ make
-# ## To install the Yarn package manager, run:
-#      curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-#      echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-#      sudo apt-get update && sudo apt-get install yarn
+# NODE / NPM
 
 # The directory /usr/local/npm and /usr/local/node_modules
 # are owned by root so a global
@@ -28,8 +14,22 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # https://www.gurustop.net/blog/2015/01/12/sudo-npm-install-g-global-mac-ubuntu-linux-windows
 # Note that ~/.npm already exists.
 npm config set prefix ~/npm
-# Makesure $HOME/npm/bin is in PATH.
 
-# Check:
-# npm bin
+# NB. Make sure $HOME/npm/bin is in PATH.
+
+# Check the value - example:
+# $ npm bin
 # /home/michael/node_modules/.bin
+
+# Disable post-install scripts for security.
+npm config set ignore-scripts true
+
+# DENO
+
+if command -v deno >/dev/null 2>&1; then
+  deno upgrade
+else
+  echo 'Installing deno'
+  curl -fsSL https://deno.land/x/install/install.sh | sh
+fi
+# NB. Make sure $HOME/.deno/bin is in PATH.

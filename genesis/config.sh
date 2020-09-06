fe97b39d874319283a3c0a76f538ebf84c33d03d
michaelcurrin <michael.currin@gmail.com>
2020-09-06 12:46:22 +0200

    Update config.sh

--- genesis/config.sh
+++ genesis/config.sh
@@ -7,14 +7,17 @@ set -e
 
 echo 'NODE / NPM'
 
-# The directory /usr/local/npm and /usr/local/node_modules
-# are owned by root so a global
-# install of a package cannot create /usr/local/npm/bin .
+# TODO figure out if this actually does what I want.
+
+# The directory /usr/local/npm and /usr/local/node_modules are owned by root so a global
+# install of a package cannot create /usr/local/npm/bin without sudo.
 # An alternative is to configure a different npm directory in the home directory.
 # https://www.gurustop.net/blog/2015/01/12/sudo-npm-install-g-global-mac-ubuntu-linux-windows
 # Note that ~/.npm already exists.
 npm config set prefix ~/npm
-# TODO does this apply independely of node version installed or get overridden?
+# TODO does this apply independently of node version installed or get overridden?
+#
+# This seems to work too without sudo - /usr/local/lib/npm/lib/node_modules
 
 # NB. Make sure $HOME/npm/bin is in PATH.
 

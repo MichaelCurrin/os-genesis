fa00fde11e09f5b7023b3322e23878940d58728c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-17 20:44:56 +0200

    docs: Update upgrade.sh

--- genesis/upgrade.sh
+++ genesis/upgrade.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# This automically accepts all upgrades.
+# In some cases you have to explicitly. I've seen this before - the first time was an error and no
+# prompt and on a subsequent run I got the y/N prompt. Therefore upgrade -y is not safe to run on a
+# cron job as it might block.
 
 echo 'Upgrade packages'
 echo
@@ -14,6 +18,14 @@ sudo apt upgrade -y -q
 echo
 echo 'GEMS'
 
+# Shared gems.
+
+# Upgrade RubyGems gem. Leave off --system to update all.
+# This doesn't cause an error status if it fails.
+# Doesn't work with --user-install
+sudo gem update --system
+
+# User gems.
 if command -c bundler >/dev/null 2>&1; then
   gem update bundler --user-install
 fi

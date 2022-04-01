b04e3a519eccabd8eef980072703fe94af280d97
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-04-01 17:44:48 +0200

    update upgrade.sh

--- genesis/other/upgrade.sh
+++ genesis/other/upgrade.sh
@@ -15,17 +15,20 @@ sudo gem update --system
 
 # Update all root gems (including system Bundler).
 #
-# There are others which are installed which I didn't install deliberately - these get updated.
+# There are others which are installed which I didn't install deliberately -
+# these get updated.
 # e.g.
 #   Gems updated:
-#     benchmark bigdecimal bundler cgi date delegate did_you_mean etc fiddle fileutils forwardable
-#     getoptlong io-console irb json logger matrix minitest mutex_m digest net-pop observer open3
-#     openssl ostruct power_assert pstore psych racc rake rdoc readline-ext reline singleton
+#     benchmark bigdecimal bundler cgi date delegate did_you_mean etc fiddle
+#     fileutils forwardable getoptlong io-console irb json logger matrix
+#     minitest mutex_m digest net-pop observer open3 openssl ostruct
+#     power_assert pstore psych racc rake rdoc readline-ext reline singleton
 #     stringio strscan test-unit timeout tracer uri xmlrpc yaml
-# I don't depend on use them directly so it maybe doesn't matter to update them as regularly.
+# I don't depend on use them directly so it maybe doesn't matter to update them
+# as regularly.
 #
-# Native extensions get built, requiring readline.h and ssl.h which were missing, therefore
-# had to install `libreadline-dev` and `libssl-dev` with APT.
+# Native extensions get built, requiring readline.h and ssl.h which were
+# missing, therefore had to install `libreadline-dev` and `libssl-dev` with APT.
 #sudo gem update
 
 # Upgrade user gems.
@@ -37,10 +40,7 @@ echo
 echo 'NPM'
 
 if command -v npm >/dev/null 2>&1; then
-  # Note: I needed NPM v6 on both mac and Linux machines, because of lockfiles for work that
-  # needs v6.
-  npm install -g npm@6
-  #npm upgrade -g npm
+  npm upgrade -g npm
 fi
 echo
 

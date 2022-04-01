77af540912a3029c39eea9d30686478ac8f42f39
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-04-01 18:02:38 +0200

    update install-optional.sh

--- genesis/apt/install-optional.sh
+++ genesis/apt/install-optional.sh
@@ -6,5 +6,6 @@ echo
 set -e
 
 # Notice from Node.js setup:
-#   To compile and install native addons from npm you may also need to install build tools:
+#   To compile and install native addons from npm you may also need to install
+#   build tools:
 sudo apt-get install -y build-essential

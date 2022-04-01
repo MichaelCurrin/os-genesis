fdc2fd3f52f877b5d40917cfe71ad1ab8a0a0de2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-04-01 17:59:01 +0200

    update config.sh

--- genesis/config.sh
+++ genesis/config.sh
@@ -7,16 +7,14 @@ set -e
 
 echo 'NODE / NPM'
 
-# Prevent sudo needed for global installs of NPM itself and other node modulers.
+# Prevent sudo needed for global installs of node modules. You'll still need
+# `sudo` access for upgrading npm itself for `/usr/local/lib/npm` and
+# `/usr/bin/npm`.
 # Note that changing owner of /usr/local is bad because sudoers needs it to be
 # owned by root.
-
-NPM_LOCAL=/usr/local/lib/npm
 NODE_MODULES_LIB=/usr/lib/node_modules
 
-for DIR in "$NPM_LOCAL" "$NODE_MODULES_LIB"; do
-  sudo mkdir -p "$DIR"
-  sudo chmod 775 "$DIR"
-done
+sudo mkdir -p "$NODE_MODULES_LIB"
+sudo chmod 775 "$NODE_MODULES_LIB"
 
 echo

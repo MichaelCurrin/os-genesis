f0aac189ec0fc8ce89322956cca49c06f0f63406
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-04-01 16:58:04 +0200

    update config.sh

--- genesis/config.sh
+++ genesis/config.sh
@@ -7,11 +7,15 @@ set -e
 
 echo 'NODE / NPM'
 
-NPM_LOCAL_LIB=/usr/local/lib/npm
+# Prevent sudo needed for global NPM installs.
+# Advice before was 'chmod 775' but I prefer to just change the owner to be me.
 
-# You can also try chown $USER.
-if [ -d "$NPM_LOCAL_LIB" ]; then
-  sudo chmod 775 "$NPM_LOCAL_LIB"
-fi
+NPM_SHARED_LIB=/usr/local/lib/npm
+MODULES_SHARED=/usr/lib/node_modules
+
+for DIR in "$NPM_SHARED_LIB" "$MODULES_SHARED"; do
+  mkdir -p "$DIR"
+  sudo chown "$USER" "$DIR"
+done
 
 echo
